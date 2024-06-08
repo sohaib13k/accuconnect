4c1e287834502b5defe38edb8f6815b5a8aa3722
Sohaib Khan <sohaibk13@gmail.com>
2024-06-08 18:14:58 +0530

    Added account app, for managing authenti. & authori.

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import CustomAccount
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+admin.site.register(CustomAccount, UserAdmin)
--- account/models.py
+++ account/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.conf import settings
 
 # Create your models here.
+gender = (
+    ("M", "Male"),
+    ("F", "Female"),
+    ("X", "Not Specified"),
+)
+
+
+class CustomAccount(AbstractUser):
+    email = models.EmailField(blank=False, null=False, unique=True)
+    gender = models.CharField(max_length=1, choices=gender, default="X")
+
+    REQUIRED_FIELDS = ["username", "first_name", "last_name"]
+    USERNAME_FIELD = "email"
+
+    def __str__(self):
+        return self.first_name
