5593a386a8e81a7a015b39fc7fdae2d84a398d78
Sohaib Khan <sohaibk13@gmail.com>
2024-06-08 17:12:11 +0530

    Below changes- 1. Added account model

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import CustomAccount, UserProfile
+from django.contrib.auth.admin import UserAdmin
+from django.contrib import admin
 
-# Register your models here.
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+
+    extra = 0
+    readonly_fields = ("friends", "sent_requests", "sent_request_count", "sent_request_tmstmp")
+
+
+class CustomAccountAdmin(UserAdmin):
+    inlines = (UserProfileInline,)
+
+
+admin.site.register(CustomAccount, CustomAccountAdmin)
--- account/models.py
+++ account/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.conf import settings
 
-# Create your models here.
+gender = (
+    ("M", "Male"),
+    ("F", "Female"),
+    ("X", "Not Specified"),
+)
+
+
+class CustomAccount(AbstractUser):
+    email = models.EmailField(blank=False, null=False, unique=True)
+    gender = models.CharField(max_length=1, choices=gender, default="X")
+
+    REQUIRED_FIELDS = ["username", "first_name", "last_name"]
+    USERNAME_FIELD = "email"
+
+    def __str__(self):
+        return self.first_name
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        CustomAccount, on_delete=models.CASCADE, related_name="profile"
+    )
+    friends = models.ManyToManyField("self")
+    sent_requests = models.ManyToManyField("self")
+    pending_requests = models.ManyToManyField("self")
+    sent_request_count = models.IntegerField(default=0)
+    sent_request_tmstmp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.first_name}'s Profile"
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL, dispatch_uid="570db2c4-59ea-4533-b65f-e2c02fec1454")
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    instance.profile.save()
